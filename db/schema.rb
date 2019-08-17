031c218c93e9019dcf0f321c30ea22fe5ff2e2b1
regilfillan <regilfillan@gmail.com>
2019-08-17 18:16:16 -0400

    created create_posts migration and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_194736) do
+ActiveRecord::Schema.define(version: 2019_08_17_221259) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
