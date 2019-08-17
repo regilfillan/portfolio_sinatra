a5c55a0f29bd2005d7ec6300e486c917348aba47
regilfillan <regilfillan@gmail.com>
2019-08-17 15:43:07 -0400

    committing forgotten files

--- config/env.rb
+++ config/env.rb
@@ -7,4 +7,10 @@ configure :development do
     set :database, 'sqlite3:db/database.db'
 end
 
+if ENV['SINATRA_ENV']= 'development'
+    require_relative "../secrets.rb"
+end
+
+require_relative '../constants.rb'
+
 require_all "app"
