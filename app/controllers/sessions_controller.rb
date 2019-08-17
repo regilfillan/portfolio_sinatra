4a4f7285b87be262dab103e47a89336f491045a1
regilfillan <regilfillan@gmail.com>
2019-08-17 17:45:33 -0400

    fixed truthy route for user and password authentication and created posts controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
 
     post '/login' do 
         user= User.find_by(username: params[:username])
-        if !!user && user.authenticate(params[:password]))
+        if !!user && user.authenticate(params[:password])
             session[:user_id]= user.id
-            redirect :"/posts/index"
+            redirect '/posts'
         else
             @failed= true
             erb :'sessions/login'
