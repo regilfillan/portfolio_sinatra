ab7b53c8a4752456bef2af3f303b9de72ec96410
regilfillan <regilfillan@gmail.com>
2019-08-17 16:43:25 -0400

    redirect if user is nil, else start over at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,12 @@ class SessionsController < ApplicationController
 
     post '/login' do 
         user= User.find_by(username: params[:username])
-        session[:user_id]= user.id
+        if !!user
+            session[:user_id]= user.id
+            redirect :"/posts/index"
+        else
+            erb :'sessions/login'
+        end
     end
 
     get "/signup" do 
