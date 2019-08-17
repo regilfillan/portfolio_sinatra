dc100735f7770d633a0060ff046c569ff709039e
regilfillan <regilfillan@gmail.com>
2019-08-17 16:34:12 -0400

    validation of name and username in User class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
 
     get "/login" do
+        erb :'sessions/login'
+    end
 
+    post '/login' do 
+        user= User.find_by(username: params[:username])
     end
 
     get "/signup" do 
-
+        erb :'sessions/signup'
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    
+    validates :name, presence: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
 
 end
