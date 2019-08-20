553e004cb1c320df314bece408cc47c8955a7d79
regilfillan <regilfillan@gmail.com>
2019-08-19 20:51:31 -0400

    confirmation true for password, sign up form

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,4 @@
 -Allow user login (sessions)
--Set up CSS
 -/signin and /login page
 -Create user in db and model
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
     validates :name, presence: true
     validates :username, presence: true, uniqueness: true
-    validates :password, length: {in: 6..20}
+    validates :password, length: {in: 6..20}, confirmation: true, unless: -> {|u| u.password.}
 
 end
