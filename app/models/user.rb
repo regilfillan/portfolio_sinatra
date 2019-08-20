bae5aa32b9c3b5aae923cc42f9f090c95a91af72
regilfillan <regilfillan@gmail.com>
2019-08-19 21:35:45 -0400

    fixed confirm password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates_presence_of :password, :on => :create
     has_many :posts
 
     validates :name, presence: true
     validates :username, presence: true, uniqueness: true
-    validates :password, length: {in: 6..20}, confirmation: true, unless: -> (u) { u.password.blank? }
-    validates :password_confirmation, presence: true
+    validates :password, length: {in: 6..20}, on: :create 
+    validates :password_confirmation, presence: true, on: :create
 
 end
